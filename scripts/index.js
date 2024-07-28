436e058de61964ac95f07d9675503dfbd7a58601
beschetnova <bes4etnova.an@yandex.ru>
2024-07-28 21:16:44 +0400

    исправлено после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
+const placesList = document.querySelector('.places__list');
+
 // Функция создания карточки
 function createCard (data, deleteCard) {
     const cardTemplate = document.querySelector('#card-template').content; 
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
     cardElement.querySelector('.card__image').src = data.link;
+    cardElement.querySelector('.card__image').alt = data.alt;
     cardElement.querySelector('.card__title').textContent = data.name;
 
     const deleteButton = cardElement.querySelector(".card__delete-button");
@@ -14,9 +17,7 @@ function createCard (data, deleteCard) {
 
 // Вывод карточки на страницу
 initialCards.forEach(card => {
-    const placesList = document.querySelector('.places__list');
     const cardElement = createCard(card, deleteCard);
-    
     placesList.append(cardElement);
   });
 
